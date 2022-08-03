ebebcb49d85e914a3c7555ac617b407cab073e80
Alexandru-Gurau <alexandru.gurau@live.com>
2022-08-03 12:43:05 +0300

    add to netlify

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,11 +1,9 @@
-import { Outlet } from 'react-router-dom';
 import './home.styles.scss';
 
 const Home = () => {
   return (
     <div>
       <h1>This is a home page</h1>
-      {/* <Outlet /> */}
     </div>
   );
 };
