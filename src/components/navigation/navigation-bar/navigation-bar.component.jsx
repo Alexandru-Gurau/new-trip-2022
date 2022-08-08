ebc5166cea01bdb3fc696b08fb2045671768d472
Alexandru-Gurau <alexandru.gurau@live.com>
2022-08-08 13:19:36 +0300

    adding logo img and hamburger menu button

--- src/components/navigation/navigation-bar/navigation-bar.component.jsx
+++ src/components/navigation/navigation-bar/navigation-bar.component.jsx
@@ -1,25 +1,41 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.svg';
+import menu from '../../../assets/menu.svg';
 
 import './navigation-bar.styles.scss';
 
 const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
   return (
     <div className='navigation__bar'>
       <div className='navigation__bar--logo'>
-        <Link to='/'>
-          <p className='text-white'>LOGO</p>
+        <Link to='/' className='logo__container'>
+          <img src={logo} alt='logo' className='logo' />
         </Link>
       </div>
       <div className='navigation__bar--links'>
         <Link to='/trips'>
-          <p className='text-white'>Book now</p>
+          <p className='text-white navigation__bar--link'>Book now</p>
         </Link>
         <Link to='/gallery'>
-          <p className='text-white'>Galery</p>
+          <p className='text-white navigation__bar--link'>Galery</p>
         </Link>
         <Link to='/contact'>
-          <p className='text-white'>Contact</p>
+          <p className='text-white navigation__bar--link'>Contact</p>
         </Link>
+        <img
+          src={menu}
+          alt='menu button'
+          className='menu__btn'
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
