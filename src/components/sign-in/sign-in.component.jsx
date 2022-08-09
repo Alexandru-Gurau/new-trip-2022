10fbbed369e9ff28a97d7671707af07ff52d6e42
Alexandru-Gurau <alexandru.gurau@live.com>
2022-08-09 16:31:04 +0300

    login and signup

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,32 +1,76 @@
 import './sign-in.styles.scss';
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../../context/user.context';
+import FormInput from '../form-input/form-input.component';
+
+import {
+  signInAuthUserWithEmailAndPassword,
+  signOutUser,
+} from '../../utils/firebase.utils';
 
 import Button from '../button/button.component';
 
-const SignIn = () => {
-  let { currentUser } = useContext(UserContext);
+const defaultFormFields = {
+  email: '',
+  password: '',
+};
 
-  const handleClick = () => {
-    console.log(currentUser);
+const SignIn = () => {
+  const { currentUser } = useContext(UserContext);
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
   };
-  const logOut = () => {
-    console.log(currentUser);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await signInAuthUserWithEmailAndPassword(email, password);
+      resetFormFields();
+      console.log('User: ', currentUser);
+      console.log('Email: ', email);
+    } catch (error) {
+      console.log('user sign in failed', error);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
   };
 
   return currentUser === null ? (
     <div className='signin'>
       <h6 className='header-small'>LOG IN</h6>
-      <input placeholder='email'></input>
-      <input placeholder='password'></input>
-      <Button onClick={() => handleClick()}>LOGIN</Button>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+        <Button type='submit'>LOGIN</Button>
+      </form>
     </div>
   ) : (
     <div className='signin'>
-      <h6 className='header-small'>HELLO, {currentUser.toUpperCase()}</h6>
-
-      <Button onClick={() => logOut()}>SIGN OUT</Button>
+      <h6 className='header-small'>HELLO </h6>
+      <Button onClick={() => signOutUser()}>SIGN OUT</Button>
     </div>
   );
 };
--- src/routes/register/register.page.jsx
+++ src/routes/register/register.page.jsx
@@ -1,18 +1,104 @@
 import './register.styles.scss';
+import { useState } from 'react';
+import FormInput from '../../components/form-input/form-input.component';
 import Button from '../../components/button/button.component';
 
-const RegisterPage = () => (
-  <div className='register-page'>
-    <div className='register-page__container'>
-      <h2 className='header-normal align-center'>
-        Sign up with your email and password
-      </h2>
-      <input type='text' placeholder='Display name' />
-      <input type='email' placeholder='Email' />
-      <input type='password' placeholder='Password' />
-      <input type='password' placeholder='Confirm Password' />
-      <Button>SIGN UP</Button>
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase.utils';
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+const RegisterPage = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert('passwords do not match');
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Cannot create user, email already in use');
+      } else {
+        console.log('user creation encountered an error', error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className='register-page'>
+      <div className='register-page__container'>
+        <h2 className='header-normal align-center'>
+          Sign up with your email and password
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <FormInput
+            label='Display Name'
+            type='text'
+            required
+            onChange={handleChange}
+            name='displayName'
+            value={displayName}
+          />
+
+          <FormInput
+            label='Email'
+            type='email'
+            required
+            onChange={handleChange}
+            name='email'
+            value={email}
+          />
+
+          <FormInput
+            label='Password'
+            type='password'
+            required
+            onChange={handleChange}
+            name='password'
+            value={password}
+          />
+
+          <FormInput
+            label='Confirm Password'
+            type='password'
+            required
+            onChange={handleChange}
+            name='confirmPassword'
+            value={confirmPassword}
+          />
+          <Button type='submit'>Sign Up</Button>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default RegisterPage;
