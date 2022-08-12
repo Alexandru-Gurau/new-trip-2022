97e147e4cd63edba4cbf9a08970e2523b6e67d37
Alexandru-Gurau <alexandru.gurau@live.com>
2022-08-12 10:35:01 +0300

    footer upgrade

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 // Routes & Components
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-import Gallery from './routes/gallery/gallery.page';
+
 import Contact from './routes/contact/contact.page';
 import TripsPage from './routes/trips/trips.page';
 import CheckoutPage from './routes/checkout/checkout.page';
@@ -17,7 +17,6 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='/gallery' element={<Gallery />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/trips' element={<TripsPage />} />
         <Route path='/register' element={<RegisterPage />} />
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,5 +1,69 @@
 import './footer.styles.scss';
+import github from '../../assets/github.svg';
+import facebook from '../../assets/facebook.svg';
+import telegram from '../../assets/telegram.svg';
+import protonmail from '../../assets/protonmail.svg';
 
-const Footer = () => <div className='footer'></div>;
+const Footer = () => (
+  <div className='footer'>
+    <div className='footer__box'>
+      <h2 className='header-normal text-white'>Contact me at</h2>
+      <div className='footer__contact'>
+        <div className='footer__contact--item'>
+          <a
+            href='https://github.com'
+            target='_blank'
+            rel='noreferrer'
+            className='contact--link'
+          >
+            <img src={github} alt='empty bag' className='contact--icon' />
+            <p className='paragraph-normal'>Github</p>
+          </a>
+        </div>
+        <div className='footer__contact--item'>
+          <a
+            href='https://ro-ro.facebook.com/'
+            target='_blank'
+            rel='noreferrer'
+            className='contact--link'
+          >
+            <img src={facebook} alt='empty bag' className='contact--icon' />
+            <p className='paragraph-normal'>Facebook</p>
+          </a>
+        </div>
+        <div className='footer__contact--item'>
+          <a
+            href='https://telegram.org/'
+            target='_blank'
+            rel='noreferrer'
+            className='contact--link'
+          >
+            <img src={telegram} alt='empty bag' className='contact--icon' />
+            <p className='paragraph-normal'>Telegram</p>
+          </a>
+        </div>
+        <div className='footer__contact--item'>
+          <a
+            href='https://proton.me/'
+            target='_blank'
+            rel='noreferrer'
+            className='contact--link'
+          >
+            <img src={protonmail} alt='empty bag' className='contact--icon' />
+            <p className='paragraph-normal'>Protonmail</p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className='footer__box'>
+      <p className='paragraph-big text-white'>
+        This site is created by Gurau Alexandru
+      </p>
+      <p className='paragraph-big text-white'>
+        All rights are reserved ®08.2022
+      </p>
+    </div>
+  </div>
+);
 
 export default Footer;
